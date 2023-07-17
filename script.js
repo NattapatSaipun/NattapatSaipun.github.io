9bfeffb923b62eea95d5001dbcb51e834d6c37c0
NattapatSaipun <nattapat25245@hotmail.com>
2023-07-17 23:39:27 +0700

    midWayFinish

--- script.js
+++ script.js
@@ -46,3 +46,12 @@ ScrollReveal().reveal('.home-content, .heading',{ origin: 'top' });
 ScrollReveal().reveal('.home-img, .skill-container, .portforio-box , .contact form',{ origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img',{ origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content',{ origin: 'right' });
+
+/*=============================typed js=============================*/ 
+const typed = new Typed('.multiple-text',{
+    strings: ['Unity Game Developer','Frontend Developer','3D Modeler','2D Artist','Editer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
