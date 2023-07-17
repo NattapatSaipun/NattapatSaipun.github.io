584b7bac17dbcfad3c2812de59e89f1186e34e9f
NattapatSaipun <nattapat25245@hotmail.com>
2023-07-17 21:06:41 +0700

    breakpoint

--- script.js
+++ script.js
@@ -16,4 +16,8 @@ window.onscroll = () => {
             });
         };
     });
+    /*=============================sticky navbar=============================*/ 
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY > 100);
 };
